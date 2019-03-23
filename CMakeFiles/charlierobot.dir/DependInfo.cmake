2329fecb10e9301d5044e9e1eea6249abb84a2cf
mrchazaaa <charlie.howlett@live.co.uk>
2019-03-23 15:38:12 +0000

    finally got the SPEED DREAMS robot to work. At this point, just a copy of the dandroid robot.

---
+++ CMakeFiles/charlierobot.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/ClothoidPath.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/ClothoidPath.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/LinePath.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/LinePath.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/MyTrack.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/MyTrack.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/Utils.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/Utils.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/charlierobot.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/charlierobot.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/cubic.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/cubic.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/danpath.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/danpath.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/driver.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/driver.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/opponent.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/opponent.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/pidcontroller.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/pidcontroller.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/pit.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/pit.cpp.o"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/src/spline.cpp" "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/drivers/charlierobot/CMakeFiles/charlierobot.dir/src/spline.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GL_GLEXT_PROTOTYPES"
+  "HAVE_CONFIG_H"
+  "SDL_FORCEFEEDBACK"
+  "SDL_JOYSTICK"
+  "SHM"
+  "SPEED_DREAMS"
+  "THIRD_PARTY_EXPAT"
+  "TRACE_LEVEL=5"
+  "TRACE_OUT"
+  "UNLOAD_SSGGRAPH"
+  "USE_RANDR_EXT"
+  "WEBSERVER"
+  "_DEFAULT_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "src/interfaces"
+  "src/libs/learning"
+  "src/libs/math"
+  "src/libs/portability"
+  "src/libs/robottools"
+  "src/libs/tgf"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/libs/robottools/CMakeFiles/robottools.dir/DependInfo.cmake"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/libs/tgfclient/CMakeFiles/tgfclient.dir/DependInfo.cmake"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/libs/tgf/CMakeFiles/tgf.dir/DependInfo.cmake"
+  "/home/mrchazaaa/workspace/third-year-project/work/SPEEDDREAMS/src/libs/portability/CMakeFiles/portability.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/charlierobot.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/charlierobot.dir/src/ClothoidPath.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/cubic.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/charlierobot.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/danpath.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/driver.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/LinePath.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/MyTrack.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/opponent.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/pidcontroller.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/pit.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/spline.cpp.o"
+  "CMakeFiles/charlierobot.dir/src/Utils.cpp.o"
+  "../../../lib64/games/speed-dreams-2/drivers/charlierobot/charlierobot.pdb"
+  "../../../lib64/games/speed-dreams-2/drivers/charlierobot/charlierobot.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/charlierobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
